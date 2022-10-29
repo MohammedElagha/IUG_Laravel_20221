786d82649210c45cdcb09e3c4f7fc1fa942ea1e3
magha <dev.magha@gmail.com>
2022-10-29 20:02:40 +0300

    customization of relationship

--- app/Http/Controllers/StudentRegisterationController.php
+++ app/Http/Controllers/StudentRegisterationController.php
@@ -8,8 +8,15 @@ use App\Models\Student;
 class StudentRegisterationController extends Controller
 {
     public function show ($student_id) {
-        $student = Student::with(['registered_courses', 'registered_courses.course'])->find($student_id);
-        // dd($student->toArray());
+        $student = Student::with(['registered_courses' => function ($query) {
+                                $query->where('grade', '>=', 60)
+                                        ->orderBy('grade', 'DESC')
+                                        ->select('*');
+                            }])
+                            ->with('registered_courses.course')
+                            ->select('*')
+                            ->find($student_id);
+        dd($student->toArray());
         return view('students.regiser')->with('student', $student);
     }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -11,9 +11,13 @@ class Student extends Model
     use HasFactory;
     use SoftDeletes;
 
+    protected $table = 'students';
+
 
     public function registered_courses () {
-        return $this->hasMany('App\Models\RegisteredCourse');
+        return $this->hasMany('App\Models\RegisteredCourse' /* , foriegn key name, primary key name */);
+
+        # registered_courses.student_id -> students.id
     }
 
     # Student 1-M RegisteredCourse
